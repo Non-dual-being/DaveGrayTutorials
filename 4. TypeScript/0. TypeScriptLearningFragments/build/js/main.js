95862ca6bcfce3b021909acdfe6550c65fcdf4c7
Non-dual-being <kevindeschepper@hotmail.com>
2025-03-03 21:53:16 +0100

    typescript learning

---
+++ 4. TypeScript/0. TypeScriptLearningFragments/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let username = "Foufke";
+let a = 12;
+let b = 6;
+let c = 2;
+let album;
+album = '6';
+let albumLabel;
+albumLabel = 6;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ 4. TypeScript/0. TypeScriptLearningFragments/src/main.ts
@@ -0,0 +1,20 @@
+let username: string = "Foufke";
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+let album: any;
+album = '6';
+
+let albumLabel: string | number;
+albumLabel = 6;
+
+const sum = (a: number, b: number) => {
+    return a + b
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g
+

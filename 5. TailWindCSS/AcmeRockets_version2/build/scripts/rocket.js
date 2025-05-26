725c918030e405a62b7f8ab6b48a7993496366dd
Non-dual-being <kevindeschepper@hotmail.com>
2025-05-26 20:02:52 +0200

    different mobile menu

---
+++ 5. TailWindCSS/AcmeRockets_version2/build/scripts/rocket.js
@@ -0,0 +1,18 @@
+const initApp = () => {
+    const hamburgerOpenMenuButton = document.getElementById('hamburger-button') ?? null;
+    const mobileNavMenu = document.getElementById('mobile-menu') ?? null;
+
+    const toggleMobileMenu = () => {
+        mobileNavMenu.classList.toggle('hidden');
+        mobileNavMenu.classList.toggle('flex');
+    }
+
+    hamburgerOpenMenuButton.addEventListener('click', () => {toggleMobileMenu()});
+    mobileNavMenu.addEventListener('click', () => {toggleMobileMenu()});
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+})
+
